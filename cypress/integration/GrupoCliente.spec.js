593f3dc20638fe8f0b1674af09a8c7ae25bcfef9
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2021-11-25 00:30:10 -0300

    aaaaaa

--- cypress/integration/GrupoCliente.spec.js
+++ cypress/integration/GrupoCliente.spec.js
@@ -2,42 +2,80 @@
 // const faker = require('faker')
 import loc from '../support/locators'
 
-context('Testar o Login', () => {
-    beforeEach(() => {
-        cy.visit('https://erp-beta.golerp.com.br/#!/compras/cotacao-compra/cadastrar');
-        cy.get(loc.login.user).type(`${Cypress.env('usuario')}`)//variavel e locators
-        cy.get(loc.login.password).clear().type(`${Cypress.env('senha')}`)//variavel e locators  
-        cy.get("[class=ng-scope]").should("contain", "ACESSAR")
-        cy.contains('ACESSAR').click({timeout: 20000})
-        cy.contains('Selecionar...').click({timeout: 10000})
-        cy.contains('DEMONSTRAÇÃO').click({timeout: 10000})
-        cy.contains('SELECIONAR FILIAL').click({timeout: 10000})
-        cy.get("[class=ng-binding]").should("contain", "Inicio");
-        //abrir a navigator bar
-        cy.get(loc.menu.navigator).click()//locators 
-        cy.wait(500)
-        cy.get(loc.menu.fixar).click()//locators
+context('GRUD - Grupo Clientes' , () => {
+  
+  beforeEach(() => {
+    cy.visit(loc.ambiente.demo);   
+    
+  })
+  
+  it('Login', () =>{
 
-        
- })
+    
+
+      cy.get(loc.login.user).type(`${Cypress.env('usuario')}`)//variavel e locators
+      cy.get(loc.login.password).clear().type(`${Cypress.env('senha')}`)//variavel e locators  
+      cy.get("[class=ng-scope]").should("contain", "ACESSAR")
+      cy.contains('ACESSAR').click({ force: true })
+      cy.wait(2000)
+     
+     //ESSA PARTE UTILIZA COM MULTI AMBIENTES DEMOSTRAÇÃO
+     cy.contains('Selecionar...').click({ force: true })
+      cy.contains('CONTROLE DE QUALIDADE AUTOMAÇÃO').click({ force: true })
+      cy.contains('SELECIONAR FILIAL').click({ force: true })
+      cy.wait(2000)
+      cy.get("[class=ng-binding]").should("contain", "Inicio");
+      //abrir a navigator bar
+      // cy.get(loc.menu.navigator).click()//locators 
+      // cy.wait(500)
+      // cy.get(loc.menu.fixar).click()//locators
+  
+
+  
+
+
+  })
      it('listagem - Criar, abrir, Ler e voltar - Grupo de Cliente', () => {
-        //seleciona cliente no menu
-        cy.get(loc.integracao.icone).click()
-        cy.get(loc.integracao.grupoCliente).click()
-        cy.get(loc.menu.caminho).should('contain', 'Grupo de Cliente')
-        //Fechar navigator bar
-        cy.get(loc.menu.fixar).click({timeout: 10000})
-        cy.wait(1000)
-        //verifica acentuação 
-        cy.get('.panel').should('contain', 'Descrição')
-        cy.get('.panel').should('contain', 'Tabela de venda')
-        cy.get('.panel').should('contain', 'Últ. Atualização')
-        //adicionar 
-        cy.get(loc.integracao.adicionar).click()
+             //abrir a navigator bar
+             if (cy.get('#navigation-menu').should("be.visible")) {
+              cy.get('#main').click(300, 30)
+              cy.get(loc.menu.navigator).click({ force: true }); //locators
+              cy.wait(500);
+              cy.get(loc.menu.fixar).click({ force: true }); //locators
+              cy.wait(500);
+              //seleciona cliente no menu
+              cy.get(loc.integracao.icone).click({ force: true });
+              cy.wait(1000);
+              cy.get(loc.integracao.grupoCliente).click({ force: true });
+              cy.wait(5000);
+             // cy.get(loc.menu.caminho).should("contain", "");
+              cy.wait(500);
+              //Fechar navigator bar
+              cy.get(loc.menu.fixar).click({ force: true });
+              cy.wait(2000);
+              } else {
+           
+                cy.get(loc.menu.navigator).click({ force: true }); //locators
+                cy.wait(500);
+                cy.get(loc.menu.fixar).click({ force: true }); //locators
+                cy.wait(500);
+                //seleciona cliente no menu
+                cy.get(loc.integracao.icone).click({ force: true });
+                cy.wait(1000);
+                cy.get(loc.integracao.grupoCliente).click({ force: true });
+                cy.wait(5000);
+              //  cy.get(loc.menu.caminho).should("contain", "");
+                cy.wait(500);
+                //Fechar navigator bar
+                cy.get(loc.menu.fixar).click({ force: true });
+                cy.wait(2000);
+              } 
+    
+        cy.get(loc.integracao.adicionar).click({ force: true })
         cy.get('.breadcrumb > :nth-child(4) > .ng-binding').should('contain', 'Cadastrar')
-        cy.get('#descricao').clear().type("Athos Teste")
+        cy.get('.form-group > .form-control').clear().type("Athos Teste")
         cy.get('.ui-select-match > .form-control').type('10% ESPECIAL','{enter}')
-        cy.get('#ui-select-choices-row-1-0 > .ui-select-choices-row-inner').click()
+         cy.get('.ui-select-choices-row-inner > .text-truncate').click()
         cy.get('#btn-salvar > span.ng-scope').click()
         cy.get(loc.menu.aviso).should('contain', 'sucesso')
         cy.get(loc.menu.fechaaviso).click({timeout: 10000})
@@ -51,21 +89,46 @@ context('Testar o Login', () => {
         cy.get(loc.menu.caminho).should('contain', 'Grupo de Cliente')
     })
 
-        it('listagem - Editar, abrir e Ler- Grupo de Cliente', () => {
-        //seleciona cliente no menu
-        cy.get(loc.integracao.icone).click()
-        cy.get(loc.integracao.grupoCliente).click()
-        cy.get(loc.menu.caminho).should('contain', 'Grupo de Cliente')
-        //Fechar navigator bar
-        cy.get(loc.menu.fixar).click({timeout: 10000})
-        cy.wait(1000)
-    
+    it('listagem - Editar, abrir e Ler- Grupo de Cliente', () => {
+          if (cy.get('#navigation-menu').should("be.visible")) {
+            cy.get('#main').click(300, 30)
+            cy.get(loc.menu.navigator).click({ force: true }); //locators
+            cy.wait(500);
+            cy.get(loc.menu.fixar).click({ force: true }); //locators
+            cy.wait(500);
+            //seleciona cliente no menu
+            cy.get(loc.integracao.icone).click({ force: true });
+            cy.wait(1000);
+            cy.get(loc.integracao.grupoCliente).click({ force: true });
+            cy.wait(5000);
+           // cy.get(loc.menu.caminho).should("contain", "");
+            cy.wait(500);
+            //Fechar navigator bar
+            cy.get(loc.menu.fixar).click({ force: true });
+            cy.wait(2000);
+            } else {
+         
+              cy.get(loc.menu.navigator).click({ force: true }); //locators
+              cy.wait(500);
+              cy.get(loc.menu.fixar).click({ force: true }); //locators
+              cy.wait(500);
+              //seleciona cliente no menu
+              cy.get(loc.integracao.icone).click({ force: true });
+              cy.wait(1000);
+              cy.get(loc.integracao.grupoCliente).click({ force: true });
+              cy.wait(5000);
+            //  cy.get(loc.menu.caminho).should("contain", "");
+              cy.wait(500);
+              //Fechar navigator bar
+              cy.get(loc.menu.fixar).click({ force: true });
+              cy.wait(2000);
+            } 
         //editando...
         cy.contains("Athos Teste",{ matchCase: false }).click()
         cy.get('#btn-editar > span.ng-scope').click({timeout: 10000})
         cy.get('.ui-select-match > .form-control').type('ATACADO')
         cy.contains('ATACADO').click()
-        cy.get('#descricao').clear().type('TESTE EDITADO')
+        cy.get('.form-group > .form-control').clear().type('TESTE EDITADO')
         cy.get('.md-raised > span.ng-scope').click()
         cy.get(loc.menu.aviso).should('contain', 'sucesso')
         cy.get(loc.menu.fechaaviso).click({timeout: 10000})
@@ -75,16 +138,39 @@ context('Testar o Login', () => {
         cy.get('[label="Identificação"] > .panel-body').should('contain','ATACADO',{ matchCase: false })
     })
     it('Abrir - Editar, cancelar edição e Excluir - Grupo de Cliente', () => {
+      if (cy.get('#navigation-menu').should("be.visible")) {
+        cy.get('#main').click(300, 30)
+        cy.get(loc.menu.navigator).click({ force: true }); //locators
+        cy.wait(500);
+        cy.get(loc.menu.fixar).click({ force: true }); //locators
+        cy.wait(500);
         //seleciona cliente no menu
-        cy.get(loc.integracao.icone).click()
-        cy.get(loc.integracao.grupoCliente).click()
-        cy.get(loc.menu.caminho).should('contain', 'Grupo de Cliente')
+        cy.get(loc.integracao.icone).click({ force: true });
+        cy.wait(1000);
+        cy.get(loc.integracao.grupoCliente).click({ force: true });
+        cy.wait(5000);
+    //    cy.get(loc.menu.caminho).should("contain", "");
+        cy.wait(500);
         //Fechar navigator bar
-        cy.get(loc.menu.fixar).click({timeout: 10000})
-        cy.wait(1000)
-        //verifica acentuação 
-        cy.get('.panel').should('contain', 'Descrição')
-        cy.get('.panel').should('contain', 'Últ. Atualização')
+        cy.get(loc.menu.fixar).click({ force: true });
+        cy.wait(2000);
+        } else {
+     
+          cy.get(loc.menu.navigator).click({ force: true }); //locators
+          cy.wait(500);
+          cy.get(loc.menu.fixar).click({ force: true }); //locators
+          cy.wait(500);
+          //seleciona cliente no menu
+          cy.get(loc.integracao.icone).click({ force: true });
+          cy.wait(1000);
+          cy.get(loc.integracao.grupoCliente).click({ force: true });
+          cy.wait(5000);
+     //     cy.get(loc.menu.caminho).should("contain", "");
+          cy.wait(500);
+          //Fechar navigator bar
+          cy.get(loc.menu.fixar).click({ force: true });
+          cy.wait(2000);
+        } 
         
         //editando...
         cy.contains("TESTE EDITADO",{ matchCase: false }).dblclick()
@@ -104,20 +190,43 @@ context('Testar o Login', () => {
 
     })
     it('listagem - Criar, e Excluir - Grupo de Cliente', () => {
+      if (cy.get('#navigation-menu').should("be.visible")) {
+        cy.get('#main').click(300, 30)
+        cy.get(loc.menu.navigator).click({ force: true }); //locators
+        cy.wait(500);
+        cy.get(loc.menu.fixar).click({ force: true }); //locators
+        cy.wait(500);
         //seleciona cliente no menu
-        cy.get(loc.integracao.icone).click()
-        cy.get(loc.integracao.grupoCliente).click()
-        cy.get(loc.menu.caminho).should('contain', 'Grupo de Cliente')
+        cy.get(loc.integracao.icone).click({ force: true });
+        cy.wait(1000);
+        cy.get(loc.integracao.grupoCliente).click({ force: true });
+        cy.wait(5000);
+     //   cy.get(loc.menu.caminho).should("contain", "");
+        cy.wait(500);
         //Fechar navigator bar
-        cy.get(loc.menu.fixar).click({timeout: 10000})
-        cy.wait(1000)
-        //verifica acentuação 
-        cy.get('.panel').should('contain', 'Descrição')
-        cy.get('.panel').should('contain', 'Últ. Atualização')
+        cy.get(loc.menu.fixar).click({ force: true });
+        cy.wait(2000);
+        } else {
+     
+          cy.get(loc.menu.navigator).click({ force: true }); //locators
+          cy.wait(500);
+          cy.get(loc.menu.fixar).click({ force: true }); //locators
+          cy.wait(500);
+          //seleciona cliente no menu
+          cy.get(loc.integracao.icone).click({ force: true });
+          cy.wait(1000);
+          cy.get(loc.integracao.grupoCliente).click({ force: true });
+          cy.wait(5000);
+   //       cy.get(loc.menu.caminho).should("contain", "");
+          cy.wait(500);
+          //Fechar navigator bar
+          cy.get(loc.menu.fixar).click({ force: true });
+          cy.wait(2000);
+        } 
 
         cy.get(loc.integracao.adicionar).click()
         cy.get('.breadcrumb > :nth-child(4) > .ng-binding').should('contain', 'Cadastrar')
-        cy.get('#descricao').clear().type("BASICO")
+        cy.get('.form-group > .form-control').clear().type("BASICO")
         cy.get('#btn-salvar > span.ng-scope').click()
         cy.get(loc.menu.aviso).should('contain', 'sucesso')
         cy.get(loc.menu.fechaaviso).click({timeout: 10000})
